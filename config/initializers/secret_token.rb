b2281741194a9e70155e26837cfe97b5c48707a1
Timothy Lynn <tlynn@nestlabs.com>
2013-09-08 16:13:43 -0700

    Initialized repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Siahl::Application.config.secret_key_base = '07dee729ebf94535664e359a771d16b4b275fbb5f9472c162ccf8be7651a3893a3e65636e5b4da92efe1dae4c547f622478cd20640de75ea35cce556f991409c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Siahl::Application.config.session_store :cookie_store, key: '_siahl_session'
